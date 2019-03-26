59d69764d4e63f8263362046dce8373a1a27eada
mvrborges <mvrborges0@gmail.com>
2019-03-26 12:34:27 -0300

    remove all cancel button and fix bug view expense and add button for start activity add expense

--- app/src/main/java/com/mvrborges/projectgold/Expense/ViewExpense.kt
+++ app/src/main/java/com/mvrborges/projectgold/Expense/ViewExpense.kt
@@ -1,13 +1,18 @@
 package com.mvrborges.projectgold.Expense
 
+import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.AdapterView
+import android.widget.Button
 import android.widget.ListView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 import com.mvrborges.projectgold.R
+import com.mvrborges.projectgold.category.AddCategory
 
 class ViewExpense : AppCompatActivity() {
     internal var list: ListView? = null
@@ -16,5 +21,13 @@ class ViewExpense : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         this.title = resources.getText(R.string.expense_title_view)
         setContentView(R.layout.activity_view_expense)
+
+        val fab = findViewById<View>(R.id.AddExpense) as FloatingActionButton
+
+
+        fab.setOnClickListener {
+            val inCreate = Intent(this@ViewExpense, AddExpense::class.java)
+            startActivity(inCreate)
+        }
     }
 }
